bdea998cd635b2ee0201af52d12a94c52f1af92b
kakaviboy291 <botdeploy365@gmail.com>
2024-08-02 07:56:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94717267602' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=aFMTETKA#35x_Ep8mhyXEC--hX9kY4Z4N33xq76R8M-_UhhhMv5g' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=aFMTETKA#35x_Ep8mhyXEC--hX9kY4Z4N33xq76R8M-_UhhhMv' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94717267602" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
